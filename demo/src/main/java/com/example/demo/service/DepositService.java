a5c6153bb7411623e09acdb4ecf2204602bd9383
pattadonkh-bit <pattadon.kh@kkumail.com>
2026-09-11 17:40:21 +0700

    Update DepositService.java

--- demo/src/main/java/com/example/demo/service/DepositService.java
+++ demo/src/main/java/com/example/demo/service/DepositService.java
@@ -1,11 +1,14 @@
 package com.example.demo.service;
 
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.example.demo.model.Account;
 import com.example.demo.model.DepositTransaction;
 import com.example.demo.repository.AccountRepository;
 import com.example.demo.repository.DepositTransactionRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DepositService {
@@ -13,8 +16,7 @@ public class DepositService {
     private final AccountRepository accountRepository;
     private final DepositTransactionRepository depositTransactionRepository;
 
-    public DepositService(AccountRepository accountRepository,
-                          DepositTransactionRepository depositTransactionRepository) {
+    public DepositService(AccountRepository accountRepository,DepositTransactionRepository depositTransactionRepository) {
         this.accountRepository = accountRepository;
         this.depositTransactionRepository = depositTransactionRepository;
     }
@@ -22,6 +24,12 @@ public class DepositService {
     @Transactional
     public void deposit(Long accountId, double amount) {
 
+        // ตรวจสอบจำนวนเงิน
+        if (amount <= 0) {
+            throw new RuntimeException("จำนวนเงินฝากต้องมากกว่า 0");
+        }
+
+        // ค้นหาบัญชี
         Account account = accountRepository.findById(accountId)
                 .orElseThrow(() -> new RuntimeException("Account not found"));
 
@@ -29,11 +37,13 @@ public class DepositService {
         account.setBalance(account.getBalance() + amount);
         accountRepository.save(account);
 
-        // บันทึก Deposit Transaction
+        // สร้างรายการฝากเงิน
         DepositTransaction transaction = new DepositTransaction();
         transaction.setAccount(account);
         transaction.setAmount(amount);
+        transaction.setTransactionDate(LocalDateTime.now());
 
+        // บันทึก Deposit Transaction
         depositTransactionRepository.save(transaction);
     }
 }
