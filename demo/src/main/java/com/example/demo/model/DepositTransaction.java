c66857042b14ed93fd9c299fd907c87b08ba6eca
pattadonkh-bit <pattadon.kh@kkumail.com>
2026-09-11 18:31:07 +0700

    Update DepositTransaction.java

--- demo/src/main/java/com/example/demo/model/DepositTransaction.java
+++ demo/src/main/java/com/example/demo/model/DepositTransaction.java
@@ -9,8 +9,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+
 @Entity
 public class DepositTransaction {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
